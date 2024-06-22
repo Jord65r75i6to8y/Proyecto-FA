3b526fa6d2eeedd74ec6710e8844f55715a8b56c
Jordan <Jordan.barquero@ucr.ac.cr>
2024-06-22 02:56:58 -0500

    Intento_de_pintar_el_bitmap

--- src/PFA.asm
+++ src/PFA.asm
@@ -1,289 +1,297 @@
 .data
 display: .space 1024
+term1: .space 3
+term2: .space 3
 .text
 
 li $t0, 0x007f1c
-li $t1, 0
 .globl main
 
-main: 
-	li $a1, 4
-	jal print_0
-	
-	li $a1, 20
-	jal print_1
-	
-	li $a1, 36
-	jal print_2
-	
-	li $a1, 52
-	jal print_3
-	
-	li $a1, 68
-	jal print_4
-	
-	li $a1, 84
-	jal print_5
-	
-	li $a1, 100
-	jal print_6
-	
-	li $a1, 116
-	jal print_7
-	
-	li $a1, 132
-	jal print_8
-	
-	li $a1, 148
-	jal print_9
-	
-	li $v0, 10
-	syscall
+main:
+    li $v0, 8         # Leer cadena
+    la $a0, term1     # Direccion de la cadena
+    li $a1, 4         # Longitud maxima de la cadena
+    syscall
+    
+    jal print
+    
+    li $v0, 10        # Salir
+    syscall
+    
+read:
+	li $t1, 0
+	jr $ra
+
+print:
+    li $t1, 0         # Inicializar indice de caracteres
+
+print_loop:
+
+    lb $t2, term1($t1)   # Cargar caracter de term1[$t1]
+    beqz $t2, end_print
+    
+    sub $t2, $t2, '0'       # Convertir ASCII a entero
+    
+    # Calcular la direcci�n en display
+    mul $t4, $t1, $a1       # Calcular el desplazamiento en display
+    add $a2, $t4, $a1       # Sumar al desplazamiento de la direccion base de display
+    
+    # Saltar a la subrutina correspondiente
+    beq $t2, 0, print_0
+    beq $t2, 1, print_1
+    beq $t2, 2, print_2
+    beq $t2, 3, print_3
+    beq $t2, 4, print_4
+    beq $t2, 5, print_5
+    beq $t2, 6, print_6
+    beq $t2, 7, print_7
+    beq $t2, 8, print_8
+    beq $t2, 9, print_9
+    
+    addi $t1, $t1, 1 # Incrementar el �ndice de caracteres
+    j print_loop
+
+end_print:
+    jr $ra
 
 print_0 :
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 8
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 8
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 8
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 8
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 8
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 8
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
 	
 	jr $ra
 	
+	
 print_1 :
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 256
-	sw $t0, display($a1)
-	addi $a1, $a1, 256
-	sw $t0, display($a1)
-	addi $a1, $a1, 256
-	sw $t0, display($a1)
-	addi $a1, $a1, 252
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 256
+	sw $t0, display($a2)
+	addi $a2, $a2, 256
+	sw $t0, display($a2)
+	addi $a2, $a2, 256
+	sw $t0, display($a2)
+	addi $a2, $a2, 252
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
 	
 	
 	jr $ra
 	
 print_2 : 
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 256
-	sw $t0, display($a1)
-	addi $a1, $a1, 252
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 256
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 256
+	sw $t0, display($a2)
+	addi $a2, $a2, 252
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 256
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
 	
 	jr $ra
 	
 print_3 : 
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 256
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 256
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 256
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 256
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
 	
 	jr $ra
 	
 print_4 :
-	sw $t0, display($a1)
-	addi $a1, $a1, 8
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 8
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 256
-	sw $t0, display($a1)
-	addi $a1, $a1, 256
-	sw $t0, display($a1)
+	sw $t0, display($a2)
+	addi $a2, $a2, 8
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 8
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 256
+	sw $t0, display($a2)
+	addi $a2, $a2, 256
+	sw $t0, display($a2)
 	
 	jr $ra
 	
 print_5:
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 256
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 256
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 256
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 256
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
 	
 	jr $ra
 
 print_6:
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 256
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 8
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 256
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 8
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
 	
 	jr $ra
 	
 print_7:
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 256
-	sw $t0, display($a1)
-	addi $a1, $a1, 252
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 252
-	sw $t0, display($a1)
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 256
+	sw $t0, display($a2)
+	addi $a2, $a2, 252
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 252
+	sw $t0, display($a2)
 	
 	
 	jr $ra
 print_8:
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 8
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 8
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 8
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 8
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
 	
 	jr $ra
 	
 print_9:
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 8
-	sw $t0, display($a1)
-	addi $a1, $a1, 248
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 4
-	sw $t0, display($a1)
-	addi $a1, $a1, 256
-	sw $t0, display($a1)
-	addi $a1, $a1, 256
-	sw $t0, display($a1)
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 8
+	sw $t0, display($a2)
+	addi $a2, $a2, 248
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 4
+	sw $t0, display($a2)
+	addi $a2, $a2, 256
+	sw $t0, display($a2)
+	addi $a2, $a2, 256
+	sw $t0, display($a2)
 	
 	jr $ra
-	
-	
-	
-
-
-
-	
